7dcf3d9a3e98012de2038ca1efb0076f13bf9cdb
Kevin Svitač <kevin@svit.ac>
2022-11-03 19:42:37 +0100

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
 [ ! -d "env" ] && python -m venv env
 
 
 source env/bin/activate
 pip install -r requirements.txt
 
-python -m mkdocs gh-deploy --force -f docs/en.yml
-python -m mkdocs gh-deploy --force -f docs/cn.yml
 
+for f in ./docs/*.yml
+do
+  python -m mkdocs build -f "$f"
+done
